73d50d084fdaba65eab2ac96a37f4cd6a50b4122
abdulkerim <abdulkerimademrcdf@gmail.com>
2023-08-23 11:17:48 -0700

    basic structure

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculator</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <div class="cont">
+        <div class="btn1">/</div>
+        <div class="btn2">*</div>
+        <div class="btn1">-</div>
+        <div class="btn1">C</div>
+        <div class="btn1">7</div>
+        <div class="btn1">8</div>
+        <div class="btn1">9</div>
+        <div class="btn1">+</div>
+        <div class="btn1">4</div>
+        <div class="btn1">5</div>
+        <div class="btn1">6</div>
+        <div class="btn1">1</div>
+        <div class="btn1">2</div>
+        <div class="btn1">3</div>
+        <div class="btn1">=</div>
+        <div class="btn1">0</div>
+        <div class="btn1">.</div>
+    </div>
+</body>
+</html>
