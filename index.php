54e0f76f5fd491767d1125086fa9ad042dd79ea6
abdulkerim <abdulkerimademrcdf@gmail.com>
2023-09-17 00:57:58 -0700

    update

--- index.php
+++ index.php
@@ -8,23 +8,40 @@
 </head>
 <body>
     <div class="cont">
-        <div class="btn1">/</div>
-        <div class="btn2">*</div>
-        <div class="btn1">-</div>
-        <div class="btn1">C</div>
-        <div class="btn1">7</div>
-        <div class="btn1">8</div>
-        <div class="btn1">9</div>
-        <div class="btn1">+</div>
-        <div class="btn1">4</div>
-        <div class="btn1">5</div>
-        <div class="btn1">6</div>
-        <div class="btn1">1</div>
-        <div class="btn1">2</div>
-        <div class="btn1">3</div>
-        <div class="btn1">=</div>
-        <div class="btn1">0</div>
-        <div class="btn1">.</div>
+        <div class="screen">
+            <input type="text">
+        </div>
+        <div id="top">
+            <div class="btn1">/</div>
+            <div class="btn2">*</div>
+            <div class="btn1">-</div>
+            <div class="btn1">C</div>
+        </div>
+
+        <div id="left">
+
+            <div class="btn1">7</div>
+            <div class="btn1">8</div>
+            <div class="btn1">9</div>
+            <div class="btn1">4</div>
+            <div class="btn1">5</div>
+            <div class="btn1">6</div>
+            <div class="btn1">1</div>
+            <div class="btn1">2</div>
+            <div class="btn1">3</div>
+        
+        </div>
+
+        <div id="right">
+            
+            <div class="btn8">+</div>
+            <div class="btn1">=</div>
+        </div>
+
+       <div id="bottom">
+            <div class="btn1">0</div>
+            <div class="btn2">.</div>
+       </div>
     </div>
 </body>
 </html>
